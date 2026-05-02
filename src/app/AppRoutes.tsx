e6923da5d251c7edff429fda29ab946726c0c753
manudesir <107033169+manudesir@users.noreply.github.com>
2026-05-02 09:08:31 +0200

    Generate catalog routes dynamically

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,12 +1,7 @@
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { ComparisonScreen } from '../modules/comparison/ComparisonScreen';
 import { useComparisonFlow } from '../modules/comparison/useComparisonFlow';
-import {
-  actionFilmCatalog,
-  defaultFilmCatalog,
-  filmItemsByCatalogId,
-  type FilmCatalog,
-} from '../modules/content/filmSource';
+import { filmCatalogs, filmItemsByCatalogId, type FilmCatalog } from '../modules/content/filmSource';
 import { RankingPage } from '../modules/ranking/RankingPage';
 import { DevDatabaseTransfer } from './DevDatabaseTransfer';
 import { isLocalDevOrigin } from './devDatabaseTransferProtocol';
@@ -28,10 +23,12 @@ export function AppRoutes() {
   return (
     <HashRouter>
       <Routes>
-        <Route path={defaultFilmCatalog.comparisonPath} element={<ComparisonPage catalog={defaultFilmCatalog} />} />
-        <Route path={defaultFilmCatalog.rankingPath} element={<RankingPage catalog={defaultFilmCatalog} />} />
-        <Route path={actionFilmCatalog.comparisonPath} element={<ComparisonPage catalog={actionFilmCatalog} />} />
-        <Route path={actionFilmCatalog.rankingPath} element={<RankingPage catalog={actionFilmCatalog} />} />
+        {filmCatalogs.map((catalog) => (
+          <Route key={catalog.comparisonPath} path={catalog.comparisonPath} element={<ComparisonPage catalog={catalog} />} />
+        ))}
+        {filmCatalogs.map((catalog) => (
+          <Route key={catalog.rankingPath} path={catalog.rankingPath} element={<RankingPage catalog={catalog} />} />
+        ))}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       {showDevDatabaseTransfer ? <DevDatabaseTransfer /> : null}
