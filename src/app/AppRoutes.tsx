c5fec0acfea84c619f2a272c8bd3bfa1e8180bd3
manudesir <107033169+manudesir@users.noreply.github.com>
2026-05-02 08:51:31 +0200

    Add default and action catalog routes

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,19 +1,40 @@
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { ComparisonScreen } from '../modules/comparison/ComparisonScreen';
 import { useComparisonFlow } from '../modules/comparison/useComparisonFlow';
+import {
+  actionFilmCatalog,
+  defaultFilmCatalog,
+  filmItemsByCatalogId,
+  type FilmCatalog,
+} from '../modules/content/filmSource';
 import { RankingPage } from '../modules/ranking/RankingPage';
 import { DevDatabaseTransfer } from './DevDatabaseTransfer';
 import { isLocalDevOrigin } from './devDatabaseTransferProtocol';
 
+type CatalogRoutesProps = {
+  catalog: FilmCatalog;
+};
+
+function CatalogRoutes({ catalog }: CatalogRoutesProps) {
+  const items = filmItemsByCatalogId[catalog.id];
+  const flow = useComparisonFlow(items);
+
+  return (
+    <>
+      <Route path={catalog.comparisonPath} element={<ComparisonScreen flow={flow} catalog={catalog} />} />
+      <Route path={catalog.rankingPath} element={<RankingPage catalog={catalog} />} />
+    </>
+  );
+}
+
 export function AppRoutes() {
-  const flow = useComparisonFlow();
   const showDevDatabaseTransfer = import.meta.env.DEV && isLocalDevOrigin(window.location.origin);
 
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<ComparisonScreen flow={flow} />} />
-        <Route path="/ranking" element={<RankingPage />} />
+        <CatalogRoutes catalog={defaultFilmCatalog} />
+        <CatalogRoutes catalog={actionFilmCatalog} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       {showDevDatabaseTransfer ? <DevDatabaseTransfer /> : null}
